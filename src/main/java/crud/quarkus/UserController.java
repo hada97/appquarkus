1f38eed4fe22f8d0a9e1535c4a10600e89f9c1d6
PC <adailson65@gmail.com>
2024-11-16 00:38:27 -0300

    add controller

---
+++ src/main/java/crud/quarkus/UserController.java
@@ -0,0 +1,22 @@
+package crud.quarkus;
+
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+
+
+@Path("/users")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class UserController {
+
+    @GET
+    public Response teste(){
+        return Response.ok("teste").build();
+    }
+
+}
